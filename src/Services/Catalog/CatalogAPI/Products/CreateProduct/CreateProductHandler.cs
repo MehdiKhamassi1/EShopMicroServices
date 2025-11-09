5591af2860bbf0ce0a1c6a58d3193cbb59bc3b28
MehdiKhamassi1 <mohamedmehdi.khamassi@esprit.tn>
2025-11-09 16:42:39 +0100

    second

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,39 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+using MediatR;
+using System.Windows.Input;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        :ICommand<CreateProdcutResult>;
+    public record CreateProdcutResult(Guid Id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProdcutResult>
+    {
+        public async Task<CreateProdcutResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // create
+            // save to db
+            // return CreateProdcutResult result
+
+            // create
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            //TODO
+            // save to db
+
+            // return CreateProdcutResult result
+            return new CreateProdcutResult(Guid.NewGuid());
+            
+        }
+    
+
+    }
+}
